b610500bbfccfafc89cd56e71fb973dd92df37c2
Daniel Miller <daniel@zoox.com>
2020-11-08 16:06:06 -0800

    Initial version complete

--- octoprint_lightsout/__init__.py
+++ octoprint_lightsout/__init__.py
@@ -1,24 +1,68 @@
-# coding=utf-8
 from __future__ import absolute_import
 
 import octoprint.plugin
+from octoprint.util import RepeatedTimer
 
 
 class LightsoutPlugin(
+    octoprint.plugin.StartupPlugin,
     octoprint.plugin.SettingsPlugin,
+    octoprint.plugin.EventHandlerPlugin,
     octoprint.plugin.AssetPlugin,
     octoprint.plugin.TemplatePlugin,
 ):
+    def __init__(self):
+        self._timer = None
+
+    ##~~ StartupPlugin mixin
+    def on_after_startup(self):
+        self._logger.info("LightsOut plugin Starting up...")
 
     ##~~ SettingsPlugin mixin
-
     def get_settings_defaults(self):
-        return dict(
-            # put your plugin's default settings here
+        return dict(timeout=60, off_gcode="M150 P0")
+
+    def get_timeout_sec(self):
+        return int(self._settings.get(["timeout"]))
+
+    def get_off_gcode(self):
+        return self._settings.get(["off_gcode"])
+
+    ##~~ EventHandlerPlugin
+    def on_event(self, event, payload):
+        self._logger.info("LightsOut received event: " + str(event))
+        if event in (
+            "Startup",
+            "SettingsUpdated",
+            "PrintFailed",
+            "PrintDone",
+            "PrintCanceled",
+        ):
+            self.restart_timer()
+        elif event in ("PrintStarted"):
+            self.stop_timer()
+
+    def restart_timer(self):
+        timeout = self.get_timeout_sec()
+        self._logger.info("LightsOut scheduling RepeatedTimer: " + str(timeout))
+
+        self.stop_timer()
+
+        self._timer = RepeatedTimer(
+            timeout, self.send_lights_off, run_first=False
         )
+        self._timer.start()
+
+    def stop_timer(self):
+        if self._timer is not None:
+            self._timer.cancel()
+
+    def send_lights_off(self):
+        self._logger.info("LightsOut plugin Shutting lights off!")
+        off_gcode = self.get_off_gcode()
+        self._printer.commands(off_gcode, tags=set(["lightsout"]))
 
     ##~~ AssetPlugin mixin
-
     def get_assets(self):
         # Define your plugin's asset files to automatically include in the
         # core UI here.
@@ -28,7 +72,36 @@ class LightsoutPlugin(
             less=["less/lightsout.less"],
         )
 
+    ##~~ TemplatePlugin mixin
+    def get_template_vars(self):
+        return dict(
+            timeout=self.get_timeout_sec(),
+            off_gcode=self.get_off_gcode(),
+        )
+
+    def get_template_configs(self):
+        return [
+            dict(type="navbar", custom_bindings=False),
+            dict(type="settings", custom_bindings=False),
+        ]
+
     ##~~ Softwareupdate hook
+    def gcode_sent_hook(
+        self,
+        comm_instance,
+        phase,
+        cmd,
+        cmd_type,
+        gcode,
+        subcode,
+        tags,
+        *args,
+        **kwargs
+    ):
+        if self.is_lights_on_gcode(cmd, cmd_type, gcode, tags):
+            self._logger.info("LightsOut detected 'ON' GCode: " + cmd)
+            self._logger.info("Restarting LightsOut timer")
+            self.restart_timer()
 
     def get_update_information(self):
         # Define the configuration for your plugin to use with the Software Update
@@ -48,10 +121,18 @@ class LightsoutPlugin(
             )
         )
 
+    def is_lights_on_gcode(self, cmd, cmd_type, gcode, tags):
+        if gcode == "M150":
+            for token in cmd.split(" "):
+                if token.startswith("P"):
+                    power_val = int(token.lstrip("P"))
+                    if power_val > 0:
+                        return True
+        return False
 
-__plugin_name__ = "LightsOut Plugin"
 
-__plugin_pythoncompat__ = ">=3,<4"  # only python 3
+__plugin_name__ = "LightsOut"
+__plugin_pythoncompat__ = ">=2.7,<4"  # Python 2 or 3
 
 
 def __plugin_load__():
@@ -60,5 +141,6 @@ def __plugin_load__():
 
     global __plugin_hooks__
     __plugin_hooks__ = {
-        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+        "octoprint.comm.protocol.gcode.sent": __plugin_implementation__.gcode_sent_hook,
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
     }
