75a937718625e5dff5fc1b2c3af3d41d128fd61f
Daniel Miller <daniel@zoox.com>
2020-11-08 19:30:47 -0800

    Cleanup before publishing

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ plugin_version = "0.1.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """TODO"""
+plugin_description = """Automatically shuts off lights after some delay when printing completes"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Daniel Miller"
